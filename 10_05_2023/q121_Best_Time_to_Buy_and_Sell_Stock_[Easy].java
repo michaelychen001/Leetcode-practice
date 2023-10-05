7195bfb2fe7d1a2318d1d08cc7c25486438364b6
Michael Chan <michaelychen001@gmail.com>
2023-10-05 17:44:14 -0400

    Solve sell stock problems

---
+++ 10_05_2023/q121_Best_Time_to_Buy_and_Sell_Stock_[Easy].java
@@ -0,0 +1,78 @@
+
+/**
+    You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+    You want to maximize your profit by choosing a single day to buy one stock and choosing a
+    different day in the future to sell that stock.
+
+    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+    Example 1:
+        Input: prices = [7,1,5,3,6,4]
+        Output: 5
+        Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+        Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+    Example 2:
+        Input: prices = [7,6,4,3,1]
+        Output: 0
+        Explanation: In this case, no transactions are done and the max profit = 0.
+
+    Ref: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=study-plan-v2&envId=top-interview-150
+*/
+
+class Solution {
+
+    public int maxProfit(int[] prices) {
+
+        int length = prices.length;
+
+        /**
+            Here we have 2 locks to make to get the max profit.brute
+            maxProfit is to make sure to obtain the overall max profit;
+            minBuyPrice is to find the lowest by price, and use this to calculate the candidates of maxProfit.
+        */
+        int maxProfit = 0;
+        int minBuyPrice = Integer.MAX_VALUE;
+
+        // One-pass approach: O(n)
+        for (int i=0; i < length; i++) {
+            if (prices[i] < minBuyPrice) {
+                minBuyPrice = prices[i];
+            } else {
+                maxProfit = java.lang.Math.max(maxProfit, prices[i] - minBuyPrice);
+            }
+        }
+
+        return maxProfit;
+
+    }
+
+    public int maxProfit_brute(int[] prices) {
+
+        int length = prices.length;
+        int maxProfit = 0;
+        int minBuyPrice = Integer.MAX_VALUE;
+
+        /** brute force approach
+         // Table
+         //  b/s    day-1, day-2, day-3, day-4
+         // day-1     0      x      x      x
+         // day-2     -      0      x      x
+         // day-3     -      -      0      x
+         // day-4     -      -      -      0
+        */
+
+        for (int i=0; i < length-1; i++) {
+            for (int j=i+1; j < length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+
+}
---
+++ 10_05_2023/q122_Best_Time_to_Buy_and_Sell_Stock_II[Medium].java
@@ -0,0 +1,67 @@
+
+/**
+    You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+    On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the
+    stock at any time. However, you can buy it then immediately sell it on the same day.
+
+    Find and return the maximum profit you can achieve.
+
+    Example 1:
+        Input: prices = [7,1,5,3,6,4]
+        Output: 7
+        Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+        Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+        Total profit is 4 + 3 = 7.
+
+    Example 2:
+        Input: prices = [1,2,3,4,5]
+        Output: 4
+        Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+        Total profit is 4.
+
+    Example 3:
+        Input: prices = [7,6,4,3,1]
+        Output: 0
+        Explanation: There is no way to make a positive profit, so we never buy the stock to achieve
+        the maximum profit of 0.
+
+    Ref: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=study-plan-v2&envId=top-interview-150
+*/
+
+class Solution {
+
+    public int maxProfit(int[] prices) {
+
+        int length = prices.length;
+        int myProfit = 0;
+        int myBuyPrice = Integer.MAX_VALUE;
+
+        // One-pass approach: O(n)
+        for (int i=0; i < length; i++) {
+            if ((i != length-1) && (prices[i] < myBuyPrice)) {
+                myBuyPrice = prices[i];
+                // System.out.println("buy it at " + String.valueOf(prices[i]));
+            }
+
+            if (i+1 < length) {
+                if (prices[i] > prices[i+1]) {
+                    // sell it
+                    myProfit += prices[i] - myBuyPrice;
+                    myBuyPrice = Integer.MAX_VALUE;
+                    // System.out.println("sell it at " + String.valueOf(prices[i]) + "; profit = " + String.valueOf(myProfit));
+                }
+            } else {
+                // the last day
+                if (prices[i] > myBuyPrice) {
+                    myProfit += prices[i] - myBuyPrice;
+                    // System.out.println("sell it at " + String.valueOf(prices[i]) + "; profit = " + String.valueOf(myProfit));
+                }
+            }
+        }
+
+        return myProfit;
+
+    }
+
+}
