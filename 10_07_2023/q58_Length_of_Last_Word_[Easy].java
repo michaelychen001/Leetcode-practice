92049c4d5f6295cb688cd9a37559c961fb2c3aee
Michael Chan <michaelychen001@gmail.com>
2023-10-08 17:27:52 -0400

    q58 completed

--- 10_07_2023/q58_Length_of_Last_Word_[Easy].java
+++ 10_07_2023/q58_Length_of_Last_Word_[Easy].java
@@ -1,17 +1,56 @@
 /**
     Questions:
+        Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+        A word is a maximal substring consisting of non-space characters only.
 
     Example 1:
+        Input: s = "Hello World"
+        Output: 5
+        Explanation: The last word is "World" with length 5.
 
     Example 2:
+        Input: s = "   fly me   to   the moon  "
+        Output: 4
+        Explanation: The last word is "moon" with length 4.
 
+    Example 3:
+        Input: s = "luffy is still joyboy"
+        Output: 6
+        Explanation: The last word is "joyboy" with length 6.
 */
 
 class Solution {
-    public void funName(string[] args) {
+    public int lengthOfLastWord(String s) {
+
         /**
-            codes go to here
+            Time-complexity: O(n)
+            Space-complexity: O(n)
         */
-        System.out.println(0);
+
+        List<Character> savedWord = new ArrayList<Character>();
+
+        int len = s.length();
+        boolean spaceFlag = false;
+
+        for (int i=0; i < len; i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                spaceFlag = true;
+            } else {
+                if (spaceFlag == true) {
+                    savedWord = new ArrayList<Character>();
+                    System.out.println("clear the list");
+                    spaceFlag = false;
+                }
+                savedWord.add(c);
+                System.out.println("saved " + String.valueOf(c) + " to list");
+            }
+        }
+
+
+        System.out.println(savedWord);
+
+        return savedWord.size();
     }
 }
